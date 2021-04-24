1bcbdd3c26849338d3e107a2608c7bb94891ebd5
Olamidun <kolapoolamidun@gmail.com>
2021-04-24 13:16:42 +0100

    set up registration endpoint

--- losales/settings.py
+++ losales/settings.py
@@ -40,8 +40,15 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework.authtoken',
-    'dj_rest_auth'
+    'dj_rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth.registration',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
