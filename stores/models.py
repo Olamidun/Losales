8c40528a7944235f869801bfe12ca9ef0793045b
Olamidun <kolapoolamidun@gmail.com>
2021-06-18 19:33:31 +0100

    hosted store images on cloudinary

--- stores/models.py
+++ stores/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.db.models import Sum, Count
+from cloudinary.models import CloudinaryField
 
 User = get_user_model()
 
@@ -10,7 +11,7 @@ class Store(models.Model):
     name = models.CharField(max_length=250)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField()
-    store_image = models.ImageField(upload_to="store_image", blank=True, null=True)
+    store_image = CloudinaryField("store_image")
     twitter_handle = models.CharField(max_length=50, blank=True, null=True)
     slug = models.SlugField(max_length=200, unique=True)
     bank_name = models.CharField(max_length=100)
