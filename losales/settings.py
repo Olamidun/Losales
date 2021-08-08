b363876cd0da9594bdfd5750a7d918c4ca7c49e2
Olamidun <kolapoolamidun@gmail.com>
2021-08-08 08:58:05 +0100

    pushed losales' dockerfile to heroku

--- losales/settings.py
+++ losales/settings.py
@@ -114,10 +114,10 @@ WSGI_APPLICATION = 'losales.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'losalesdb',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
         'PORT': 5432,  
     }
 }
